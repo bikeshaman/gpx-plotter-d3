1094c3bdaa51b4ac25c9cf060180e8432b7f551c
Adam Gipril <adam.gipril@icloud.com>
2019-07-01 23:20:35 -0600

    Webpack and ESLint configured

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'client', 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'client.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'client'),
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
