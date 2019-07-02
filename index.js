9b8433ef0bd89a841cf796a929b0128f23fababf
Adam Gipril <adam.gipril@icloud.com>
2019-07-01 23:21:15 -0600

    Client skeleton begun, run data sent from /data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const parser = require('fast-xml-parser');
+const express = require('express');
 
 const file = fs.readFileSync(path.join(__dirname, 'Long_s_Chasm_Lake.gpx'));
 
@@ -8,3 +9,16 @@ const json = parser.parse(file.toString(), {
   attributeNamePrefix: '',
   ignoreAttributes: false,
 });
+
+const { gpx: { trk: { trkseg: { trkpt: data } } } } = json;
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.static('public'));
+
+app.get('/data', (req, res) => {
+  res.json(data);
+});
+
+app.listen(PORT);
